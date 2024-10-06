0164c1ac7abaac6731d127ff9aadef6ad6657757
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-10-07 00:57:34 +0300

    now connected to database and signup integrated & working

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,29 @@
 import express, { Application } from 'express';
-
 import 'dotenv/config';
+import helmet from 'helmet';
+import connectDB from './db/db.js';
+import authRoutes from './routes/authRoutes.js';
+import { globalErrorMiddleware } from './utils/errorMiddleware.js';
 
 
 const app: Application = express();
 
 // Middleware
+app.use(helmet());
 app.use(express.json({ limit: '10mb' }));
 
 // Routes
 app.get('/', (_req, res) => {
-  res.send('Root endpoint check ');
+    res.send('Root endpoint check ');
 });
 
+// auth routes
+app.use('/api/auth', authRoutes);
+
+app.use(globalErrorMiddleware);
 
 
+// Connect to DB
+connectDB();
 
 export default app;
