16575150d6f4403d16f8f4270f379a3a671a493e
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-10-15 12:00:19 +0300

    about section done and uploaded licence free images

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import { CardDemo } from '@/components/blocks/cards-demo-3';
 import Hero from '@/sections/Hero';
+import About from '@/sections/About';
 
 const Home: React.FC = () => {
     return (
         <div className="home">
             <Hero />
+            <About />
         </div>
     );
 };
