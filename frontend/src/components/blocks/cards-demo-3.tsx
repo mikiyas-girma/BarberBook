1b7199717174a4af5559957135726182ff2cfb72
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-10-15 18:08:19 +0300

    fix: rendering of barbers image

--- frontend/src/components/blocks/cards-demo-3.tsx
+++ frontend/src/components/blocks/cards-demo-3.tsx
@@ -5,16 +5,14 @@ export function CardDemo() {
     <div className="max-w-xs w-full group/card">
       <div
         className={cn(
-          " cursor-pointer overflow-hidden relative card w-72 h-80 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
-          "bg-[url(images/image4.jpg?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+          " cursor-pointer overflow-hidden relative card w-72 h-80 rounded-md shadow-xl  max-w-sm mx-auto flex flex-col justify-between p-4",
+          "bg-[url(/images/image4.jpg?height=256&width=256)] bg-cover"
         )}
       >
         <div className="absolute w-full h-full top-0 left-0 transition duration-300 opacity-60 group-hover/card:backdrop-brightness-50 "></div>
         <div className="flex flex-row items-center space-x-4 z-10 ">
           <img
-            height="100"
-            width="100"
-            src="/images/image5.jpg"
+            src="/images/image3.jpg"
             className="h-10 w-10 rounded-full border-2 object-cover"
           />
           <div className="flex flex-col">
