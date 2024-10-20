2223bfc6df73025a5419cf4b0f91a1fed8221646
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-10-21 00:03:37 +0300

    wrote github actions for the backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
+const apiURL = "http://localhost:3004";
+ 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: apiURL,
+        secure: false,
+      },
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
-        // @/ alias
-        '@': path.resolve(__dirname, './src'),
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
